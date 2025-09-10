e5e143c7a081fbc56aef2693c40eca522f98d0a3
Llibert <llibert.777@gmail.com>
2025-09-10 17:27:00 +0200

    no message

--- src/PKllibert/ReceiptBuilder.java
+++ src/PKllibert/ReceiptBuilder.java
@@ -1,12 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package PKllibert;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
